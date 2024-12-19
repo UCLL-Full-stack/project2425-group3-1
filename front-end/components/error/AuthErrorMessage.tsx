45526d7f286657e1a0f67c6aede2e66cc4760665
AxlNuyens <axl.nuyens@student.ucll.be>
2024-12-19 11:09:24 +0100

    refactored the not logged in message after lecturer's feedback

--- front-end/components/error/AuthErrorMessage.tsx
+++ front-end/components/error/AuthErrorMessage.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "@/styles/workouts.module.css";
+import { useRouter } from "next/router";
 
 const AuthErrorMessage: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/login"); // Redirect after a delay
+    }, 1500); // 3-second delay
+
+    return () => clearTimeout(timer); // Cleanup timeout on component unmount
+  }, [router]);
+
   return (
     <div className={styles.errorContainer}>
       <p className={styles.errorMessage}>
-        You must be logged in to be able to view this page.
+        You must be logged in to view this page. Redirecting to login...
       </p>
     </div>
   );
